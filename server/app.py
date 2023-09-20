2e573dd70c04d5118d40f76f2d861323354aa323
Eric Krukowski <erickruk@amazon.com>
2023-09-20 04:09:20 -0400

    Lab complete

--- server/app.py
+++ server/app.py
@@ -1,27 +1,128 @@
 #!/usr/bin/env python3
 
-from flask import request, session
+from flask import request, session, make_response
 from flask_restful import Resource
-from sqlalchemy.exc import IntegrityError
 
 from config import app, db, api
 from models import User, Recipe
 
 class Signup(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        if (not json.get('username')):
+            resp = make_response(
+                {'error': '422 Unprocessable Content'},
+                422
+            )
+            return resp
+
+        user = User(
+            username=json['username'],
+            bio=json['bio'],
+            image_url=json['image_url']
+        )
+        user.password_hash = json['password']
+        
+        db.session.add(user)
+        db.session.commit()
+        user_dict = User.query.filter_by(username=user.username).first().to_dict()
+        resp = make_response(
+            user_dict,
+            201
+        )
+        return resp
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user = User.query.filter_by(id=session['user_id']).first()
+        if user:
+            resp = make_response(
+                user.to_dict(),
+                200
+            )
+            return resp
+        else:
+            resp = make_response(
+                {},
+                401
+            )
+            return resp
 
 class Login(Resource):
-    pass
+    def post(self):
+        json = request.get_json()
+        user = User.query.filter_by(username=json['username']).first()
+        if not user:
+            return invalid_login()
+        password = json['password']
+        if user.authenticate(password):
+            session['user_id'] = user.id
+            resp = make_response(
+                user.to_dict(),
+                200
+            )
+            return resp
+        return invalid_login()
 
+def invalid_login():
+        resp = make_response(
+            {"error": "Invalid username or password"},
+            401
+        )
+        return resp
 class Logout(Resource):
-    pass
+    def delete(self):
+        session['user_id'] = None
+        resp = make_response({}, 401)
+        return resp
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+        if not session['user_id']:
+            resp = make_response(
+                {},
+                401
+            )
+            return resp
+        
+        recipes = Recipe.query.filter_by(user_id=session['user_id']).all()
+        recipe_dict = [recipe.to_dict() for recipe in recipes]
+        resp = make_response(
+            recipe_dict,
+            200
+        )
+        return resp
+    
+    def post(self):
+        if not session['user_id']:
+            resp = make_response(
+                {"message": "User not logged in"},
+                401
+            )
+        json = request.get_json()
+        recipe = Recipe(
+            title = json['title'],
+            instructions = json['instructions'],
+            minutes_to_complete = json['minutes_to_complete'],
+            user_id = session['user_id']
+        )
+        try:
+            db.session.add(recipe)
+            db.session.commit()
 
+            recipe_dict = Recipe.query.order_by(Recipe.id.desc()).first().to_dict()
+            resp = make_response(
+                recipe_dict,
+                201
+            )
+            return resp
+        except:
+            resp = make_response(
+                {"message": "422 Unprocessable Entity"},
+                422
+            )
+            return resp
+        
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 api.add_resource(Login, '/login', endpoint='login')
--- server/models.py
+++ server/models.py
@@ -1,14 +1,59 @@
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
-
+from sqlalchemy.orm import validates
+from sqlalchemy import exc
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String, nullable = False, unique = True)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+    
+    recipes = db.relationship('Recipe', backref='user')
+
+    serialize_rules = (
+        "-recipes.user",
+    )
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+    
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8')
+        )
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8')
+        )
+    
+    def __repr__(self):
+        return f'User {self.username}, ID: {self.id}'
+
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+    __table_args__ = (
+        db.CheckConstraint('length(instructions) >= 50'),
+    )
+
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String, nullable = False)
+    instructions = db.Column(db.String, nullable = False)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     
-    pass
+    # @validates('instructions')
+    # def validate_title(self, key, instruction):
+    #     if len(instruction)<50:
+    #         raise AttributeError("Requires at least 50 characters")
+    #     return instruction
